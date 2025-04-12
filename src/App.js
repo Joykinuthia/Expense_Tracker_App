7370d3c70386d9cd18fed46b8ba49a055fde1479
Joykinuthia <joyrosekinuthia@gmail.com>
2025-04-12 19:41:23 +0300

    State management

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import './App.css';
 
 import React, { useState } from 'react'
 
-function App() {
-  // State management
-  const [expenses, setExpenses] = useState([
-    { id: 1, description: 'Shopping', amount: 15000, category: 'Utilities' },
-    { id: 2, description: 'Fuel', amount: 10000, category: 'Transport' },
-    { id: 3, description: 'Rent', amount: 30000, category: 'Essentials' },
-    { id: 4, description: 'Savings', amount: 50000, category: 'Investments' }
-  ]);
-  const [searchTerm, setSearchTerm] = useState('');
+function App() 
+// State management
+const [expenses, setExpenses] = useState([
+  { id: 1, description: 'Shopping', amount: 15000, category: 'Utilities' },
+  { id: 2, description: 'Fuel', amount: 10000, category: 'Transport' },
+  { id: 3, description: 'Rent', amount: 30000, category: 'Essentials' },
+  { id: 4, description: 'Savings', amount: 50000, category: 'Investments' }
+]);
+const [searchTerm, setSearchTerm] = useState('');
   return (
     <div>
       
